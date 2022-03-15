3522647d6b5e99f61bf6cf684a5bd9600d80d91c
PratibhaChavan <92856684+PratibhaChavan@users.noreply.github.com>
2022-03-15 21:36:32 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>SHHJB Home</title>
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <!-- Footer -->
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <!-- Custom Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+        <!-- Plugin CSS -->
+        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/creative.css" type="text/css">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body id="page-top">
+
+        <nav id="mainNav" class="navbar navbar-default navbar-fixed-top" style="background:black">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="http://www.snjb.org.com">Shri H.H.J.B.Polytechnic Chandwad</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a class="page-scroll" href="../Student/studentregistration.php"">Student Registration</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Student/studentlogin.php">Student Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Admin/adminlogin.php">Admin Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Placement/recruiterlogin.php">Recruiter Login</a>
+                        </li>
+                    </ul>
+
+            </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+        <header>
+            <div class="header-content">
+                <div class="header-content-inner">
+                    <h1> TRAINING AND PLACEMENT MANAGEMENT SYSTEM</h1>
+                    <hr>
+                    <p>We are here to Build your Skills and Career with our Driven Passion and Reality.</br>Click Below to Get Our Current Drive
+                        Details</p>
+                    <a href="../Drives/index.php" class="btn btn-primary btn-xl page-scroll">Current Drives</a>
+                </div>
+            </div>
+        </header>
+        <div class="footer">
+			<div class="container">
+				<div class="col-md-4 ftr_navi ftr">
+					<h3>NAVIGATION</h3>
+					<ul>
+						<li>
+							<a href="../Homepage/index.php">Home</a>
+						</li>
+						<li>
+							<a href="../Student/studentregistration.php">Student Registration</a>
+						</li>
+						<li>
+							<a href="../Student/studentlogin.php">Student Login</a>
+						</li>
+						<li>
+							<a href="../Admin/adminlogin.php">Admin Login</a>
+						</li>
+						<li>
+							<a href="../Placement/recruiterlogin.php">Recruiter Login</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-4 ftr_navi ftr">
+					<h3>MEMBERS</h3>
+					<ul>
+						<li>
+							<a href="#">Customer Support</a>
+						</li>
+						<li>
+							<a href="#">Placement Support</a>
+						</li>
+						<li>
+							<a href="#">Faculty Support</a>
+						</li>
+						<li>
+							<a href="#">Registered Companies</a>
+						</li>
+						<li>
+							<a href="#">Training</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-4 get_in_touch ftr">
+					<h3>GET IN TOUCH</h3>
+					<p>S.H.H.J.B.Polytechnic</p>
+					<p>Nashik,Maharastra, India</p>
+				
+					
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+    </body>
+
+</html>
